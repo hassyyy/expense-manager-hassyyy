9bd8c0fb9be32fe57eb04fb598302ab5f569922a
hassyyy <nmohamedasan@gmail.com>
2023-08-27 11:12:42 +0530

    Prevent deleting a credit card with transactions or loans

--- app/models/credit_card.rb
+++ app/models/credit_card.rb
@@ -2,6 +2,12 @@ class CreditCard < ApplicationRecord
   has_many :transactions
   has_many :loans
   default_scope { order(created_at: :asc) }
+  before_destroy :stop_destroy_if_any_associations
+
+  def stop_destroy_if_any_associations
+    return if transactions.none? && loans.none?
+    raise ActiveRecord::RecordNotDestroyed, 'Cannot delete due to existing transactions or loans'
+  end
 
   def current_month
     (Date.today.day <= billing_day) ? Date.today : Date.today.next_month

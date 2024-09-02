9bfb87e12e94050511abaadf07ad6a69d3d6c505
Hassyyy <nmohamedasan@gmail.com>
2024-09-02 21:58:47 +0530

    Fix migration

--- db/migrate/20240902000000_add_columns_to_savings.rb
+++ db/migrate/20240902000000_add_columns_to_savings.rb
@@ -1,5 +1,6 @@
 class AddColumnsToSavings < ActiveRecord::Migration[7.0]
   def change
-    add_column :savings, :investment, :lent
+    add_column :savings, :investment, :boolean
+    add_column :savings, :lent, :boolean
   end
 end

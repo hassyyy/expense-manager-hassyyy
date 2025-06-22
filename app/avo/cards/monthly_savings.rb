1b82ef2fa11da51ae5ceb05c47c38e6b13f262ff
Hassyyy <nmohamedasan@gmail.com>
2025-06-22 20:52:57 +0530

    Revert changes

--- app/avo/cards/monthly_savings.rb
+++
@@ -1,7 +0,0 @@
-class MonthlySavings < Avo::Dashboards::PartialCard
-  self.id = "monthly_savings"
-  self.partial = "avo/cards/monthly_savings"
-  self.display_header = false
-  self.rows = 12
-  self.cols = 6
-end
--- app/avo/dashboards/monthly_savings_dashboard.rb
+++
@@ -1,7 +0,0 @@
-class MonthlySavingsDashboard < Avo::Dashboards::BaseDashboard
-  self.id = "monthly_savings_dashboard"
-  self.name = "Monthly Savings"
-
-  # cards go here
-  card MonthlySavings
-end
